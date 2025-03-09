c353fc1cd2845043985d3734a3dde4e6f3f15ec8
Kuba Brzeski <soryaleni@gmail.com>
2025-03-09 21:36:26 +0100

    Zadanie 1

---
+++ PierwszeZajecia/PierwszeZajecia/PierwszeZajecia.py
@@ -0,0 +1,15 @@
+#Zadanie 1
+
+x=int(input("Podaj x:"))
+x2=x*x
+x4=x2*x2
+print()
+print(f"x4 w 2 mnozeniach to {x4}")
+
+x6 = x4 *x2
+print()
+print(f"x6 w 3 mnozeniach to {x6}")
+
+x7 = x6* x
+print()
+print(f"x7 w 4 mnozeniach to {x7}")
